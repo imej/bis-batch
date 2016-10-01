4ffb33da253be208d702110be49fb00958849f66
Richard Cao <cao_xuelei@hotmail.com>
2016-10-01 08:38:44 -0600

    add recording to db

--- src/main/java/abmi/bis/batch/dao/DBDaoImpl.java
+++ src/main/java/abmi/bis/batch/dao/DBDaoImpl.java
@@ -1,15 +1,30 @@
 package abmi.bis.batch.dao;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import abmi.bis.batch.model.CSVRow;
+import abmi.bis.batch.service.RecordingService;
 
+@Repository("dBDao")
 public class DBDaoImpl implements DBDao {
 	
 	@Autowired
 	private JdbcOperations jdbcOper;
-
+	
+	@Autowired
+	private RecordingService recordingService;
+	
 	private static final String QUERY_FIELD_DATA_ID = 
 			"SELECT field_data.fd_id FROM projects " +
             "    INNER JOIN sites ON projects.proj_id = sites.proj_id " +
@@ -21,10 +36,19 @@ public class DBDaoImpl implements DBDao {
             "    AND field_data.year = ? " +
             "    AND field_data.round = ?";
 
-	private static final String QUERY_RECORDING_BY_FILE_NAME = 
-			"SELECT record_id FROM recordings " +
+	private static final String COUNT_RECORDING_BY_FILE_NAME = 
+			"SELECT COUNT(record_id) FROM recordings " +
             "WHERE file_name = ?";
 	
+	private static final String COUNT_REPLICATE_BY_FILE_NAME_AND_NUM = 
+	        "SELECT COUNT(rep_id) FROM replicates rep " +
+            "    INNER JOIN recordings rec on rep.record_id = rec.record_id " +
+            "WHERE rec.file_name = ? AND rep.rep_num = ?";
+    
+	private static final String INSERT_RECORDING = 
+	        "INSERT INTO recordings " + 
+            "    (fd_id, record_date, record_time, file_name, file_type, file_length) " +
+            "VALUES (?, ?, ?, ?, ?, ?)";
 	
 	@Override
 	public boolean isMetaDataReaday(String project, String site, String station, int year, int round) {
@@ -41,20 +65,42 @@ public class DBDaoImpl implements DBDao {
 
 	@Override
 	public boolean recordingExists(String fileName) {
-		// TODO Auto-generated method stub
-		return false;
+        Integer i = (Integer)jdbcOper.queryForObject(COUNT_RECORDING_BY_FILE_NAME, 
+					new Object[] {fileName}, Integer.class);
+		
+		return i > 0;
 	}
 
 	@Override
 	public boolean replicateExists(String fileName, int repNum) {
-		// TODO Auto-generated method stub
-		return false;
+		Integer i = (Integer)jdbcOper.queryForObject(COUNT_REPLICATE_BY_FILE_NAME_AND_NUM, 
+				new Object[] {fileName, repNum}, Integer.class);
+	
+	    return i > 0;
 	}
 
 	@Override
 	public long addRecording(CSVRow row) {
-		// TODO Auto-generated method stub
-		return 0;
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcOper.update(
+				new PreparedStatementCreator() {
+					public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+						PreparedStatement ps = conn.prepareStatement(INSERT_RECORDING, new String[]{"record_id"});
+						
+						int index = 1;
+						ps.setLong(index++, getFieldDataId(row.getProject(), row.getSite(), row.getStation(), row.getYear(), row.getRound()));
+						ps.setDate(index++, new java.sql.Date(row.getCreated().getTime()));
+						ps.setTime(index++, new java.sql.Time(row.getCreated().getTime()));
+						ps.setString(index++, row.getFileName().substring(0, row.getFileName().length()-4));
+						ps.setString(index++, "mp3");
+						ps.setDouble(index++, recordingService.getRecordingLength(row.getFolderPath() + File.separator + row.getFileName()));
+					    return ps;
+					}
+				}, 
+				keyHolder);
+		
+		return (Long)keyHolder.getKey();
+		
 	}
 
 	@Override
@@ -70,7 +116,14 @@ public class DBDaoImpl implements DBDao {
 	}
 
 	private Long getFieldDataId(String project, String site, String station, int year, int round) {
-		return (Long)jdbcOper.queryForObject(QUERY_FIELD_DATA_ID, 
-				new Object[] {project, site, station, year, round}, Long.class);
+		Long l;
+		
+		try {
+			l = (Long)jdbcOper.queryForObject(QUERY_FIELD_DATA_ID, 
+					new Object[] {project, site, station, year, round}, Long.class);
+		} catch (EmptyResultDataAccessException e) {
+			l = -1L;
+		}
+		return l;
 	}
 }
--- src/test/java/abmi/bis/batch/test/DBDaoTests.java
+++ src/test/java/abmi/bis/batch/test/DBDaoTests.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import abmi.bis.batch.dao.DBDao;
+import abmi.bis.batch.model.CSVRow;
 
 @ContextConfiguration(classes = abmi.bis.batch.config.AppConfig.class, loader = AnnotationConfigContextLoader.class)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,12 +30,56 @@ public class DBDaoTests {
 		assertTrue("Empty project does not exist", !dbDao.isMetaDataReaday(project, site, station, year, round));
 		
 		project = "CAO";
-		site = "Exciting Site";
+		site = "ExcitingSite";
 		station = "S1";
 		year = 2008;
 		round = 10;
 		
 		assertTrue("Empty project does not exist", dbDao.isMetaDataReaday(project, site, station, year, round));
 		
+	}
+	
+	@Test
+	public void testRecordingExists() {
+		String fileName1 = "not_exist_file";
+		
+		assertTrue(fileName1 + " exists", !dbDao.recordingExists(fileName1));
+		
+		String fileName2 = "FMX-01-01-E_20140603_050000";
+		
+		assertTrue(fileName2 + " not exists", dbDao.recordingExists(fileName2));
+		
+	}
+	
+	@Test
+	public void testReplicateExists() {
+		String fileName1 = "FMX-01-01-E_20140603_050000";
+		int repNum1 = 5;
+		
+		assertTrue(fileName1 + " - " + repNum1 + " exists", !dbDao.replicateExists(fileName1, repNum1));
+		
+		int repNum2 = 1;
+		
+		assertTrue(fileName1 + " - " + repNum2 + " not exists", dbDao.replicateExists(fileName1, repNum2));
+		
+	}
+	
+	@Test
+	public void testAddRecording() {
+		CSVRow row = new CSVRow();
+		
+		row.setId(1);
+		row.setFolderPath("C:\\temp\\bis-batch\\recordings\\temp");
+		row.setFileName("CAO-ExcitingSite-S1_20160621_053400.wav");
+		row.setReplicateNumber(1);
+        row.setLANumber(100);
+        row.setMethod(11);
+        row.setObserver(1);
+		row.setYear(2008);
+		row.setRound(10);
+		
+		long l = dbDao.addRecording(row);
+		
+		assertTrue("New recording ID must > 0", l > 0);
 	}
 }
